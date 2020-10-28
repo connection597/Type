7add4bfffd744eb522c05d2737d8a45d8aa17a8b
connection579 <wlswn4118@naver.com>
2020-10-29 01:28:24 +0900

    변수의 자료형 int와 string

---
+++ src/one/Assignment.java
@@ -0,0 +1,17 @@
+package one;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		int a ; // int는 정수를 표현할때 쓰는 자료형
+		String b; // 문자열 값을 담을수 있은 자료형
+		
+		a =1;
+		b ="hello java";
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
